ab27f78a0617195af6ba67c325038caaefd444fa
Syed <sars.latest@gmail.com>
2023-11-25 14:36:34 +0400

    Beautify, EXTENSION AUTO CHANGE

--- src/Components/Reports/ReportOal/OAL_INAD_PASSENGER_REPORT/OAL_REPORT.jsx
+++ src/Components/Reports/ReportOal/OAL_INAD_PASSENGER_REPORT/OAL_REPORT.jsx
@@ -7,26 +7,29 @@ import React from "react";
 import BreadCrumbs from "../../../Global/BreadCrumbs/BreadCrumbs";
 
 // Components
-import Table from "./Table_OAL_REPORT"
+import Table from "./Table_OAL_REPORT";
 
 // Icons
-import {MdOutlineAddCircleOutline, MdRemoveCircleOutline, MdOutlineControlPointDuplicate} from 'react-icons/md'
+import {
+  MdOutlineAddCircleOutline,
+  MdRemoveCircleOutline,
+  MdOutlineControlPointDuplicate,
+} from "react-icons/md";
 
 // Assets
 
-const OAL_REPORT = ({report}) => {
+const OAL_REPORT = ({ report }) => {
   return (
     <div className="w-full">
-
       {/* Breadcrumb Basic */}
-      <BreadCrumbs list={['Reports', 'OAL Report', 'Default']}/>
+      <BreadCrumbs list={["Reports", "OAL Report", "Default"]} />
 
       {/*  */}
 
       {/* Table */}
       <div className="w-full  lg:overflow-x-scroll">
-        <Table report={report}/>
-                 </div>
+        <Table report={report} />
+      </div>
     </div>
   );
 };
